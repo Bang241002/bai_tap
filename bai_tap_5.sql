d006987f5244b6e632b23ebc202c6697b19b8728
Bang241002 <91303671+Bang241002@users.noreply.github.com>
2022-01-07 08:36:57 +0700

    Add files via upload

---
+++ bai_tap_5.sql
@@ -0,0 +1,122 @@
+﻿-- DATABASE
+create DATABASE Assignment05
+go
+
+USE Assignment05
+go
+
+
+-- Bảng DanhBa
+Create Table DanhBa(
+	MaDB int primary key,
+	HoVaTen nvarchar(50),
+	DiaChi nvarchar(100),
+	NgaySinh Date ,
+)
+
+
+
+-- Bảng SDT
+Create Table SDTs(
+	MaSDT int primary key,
+	SDT char(9),
+	MaDB int foreign key references DanhBa(MaDB),
+)
+go
+
+
+-- 3
+-- INSERT Dữ Liệu
+INSERT INTO DanhBa VALUES(1,N'Nguyễn Văn An',N'Hà Nội','1887/11/18'),
+						 (2,N'Nguyễn Cẩm Xuyến',N'Nam Định','1990/11/16'),
+						 (3,N'Nguyễn Thị Mến',N'TP.HCM','1995/09/08')
+go
+
+
+INSERT INTO SDTs VALUES(1,'987654321',1),
+					   (2,'887654321',1),
+					   (3,'787654321',1),
+					   (4,'687654321',1),
+					   (5,'587654321',2),
+					   (6,'487654321',2),
+					   (7,'387654321',3)
+go
+
+
+-- 4
+-- Liệt kê danh sách những người trong danh bạ
+Select * From DanhBa
+go
+
+-- Liệt kê danh sách số điện thoại có trong danh bạ
+Select * From SDTs
+go 
+
+
+-- 5
+-- Liệt kê danh sách người trong danh bạ theo thứ thự alphabet.
+Select * From SDTs
+Order By SDT
+go
+
+
+-- Liệt kê các số điện thoại của người có thên là Nguyễn Văn An.
+Select HoVaTen, SDT
+From DanhBa INNER JOIN SDTs
+ON DanhBa.MaDB = SDTs.MaDB
+Where HoVaTen = N'Nguyễn Văn An'
+go
+
+
+-- Liệt kê những người có ngày sinh là 1990/11/16
+Select * From DanhBa
+Where NgaySinh = '1990/11/16'
+go
+
+
+-- 6
+-- Tìm số lượng số điện thoại của mỗi người trong danh bạ.
+Select HoVaTen, SDTs.MaDB
+From DanhBa INNER JOIN SDTs
+ON DanhBa.MaDB = SDTs.MaDB
+where SDTs.MaDB IN
+(Select  COUNT(SDTs.MaDB)
+From SDTs 
+Group by SDTs.MaDB)
+ go
+
+
+-- Tìm tổng số người trong danh bạ sinh vào tháng 11.
+Select * from DanhBa
+Where DATEPART(m, NgaySinh) = 11
+go
+
+
+-- Hiển thị toàn bộ thông tin về người, của từng số điện thoại.
+Select *
+From DanhBa INNER JOIN SDTs
+ON DanhBa.MaDB = SDTs.MaDB
+go
+
+
+-- Hiển thị toàn bộ thông tin về người, của số điện thoại 587654321
+Select *
+From DanhBa left Outer JOIN SDTs
+ON DanhBa.MaDB = SDTs.MaDB
+Where SDT = '587654321'
+Go
+
+
+-- 7
+-- Viết câu lệnh để thay đổi trường ngày sinh là trước ngày hiện tại.
+Alter Table DanhBa
+Add Check(NgaySinh < getdate())
+go
+
+
+-- Viết câu lệnh để xác định các trường khóa chính và khóa ngoại của các bảng.
+--
+-- Viết câu lệnh để thêm trường ngày bắt đầu liên lạc.
+Alter Table DanhBa
+ADD ngaybatdaulienlac date 
+go
---
+++ bai_tap_6.sql
@@ -0,0 +1,132 @@
+﻿-- DATABASE
+Create DATABASE Assignment06
+go
+
+USE Assignment06
+go
+
+
+-- Bảng NhaXuatBan
+Create Table NhaXuatBan (
+	NhaXuatBanID int primary key,
+	TenNhaXuatBan nvarchar(50),
+	DiaChi nvarchar(50),
+)
+
+
+
+-- Bảng LoaiSach
+Create Table LoaiSach(
+	LoaiSachID int primary key,
+	TenLoaiSach nvarchar(50),
+)
+go
+
+
+-- Bảng Sach
+Create Table Sach(
+	SachID int primary key,
+	TenSach nvarchar(100),
+	TacGia nvarchar(50),
+	NoiDung nvarchar(200),
+	NamXuatBan char(4),
+	LanXuatBan int,
+	Gia int,
+	SoLuong int ,	
+	LoaiSachID int foreign key References LoaiSach(LoaiSachID),
+	NhaXuatBanID int foreign key References NhaXuatBan(NhaXuatBanID)
+)
+go
+
+
+-- 2 Viết lệnh SQL chèn vào các bảng của CSDL các dữ liệu mẫu
+Insert Into NhaXuatBan Values(1,N'Tri Thức',N'Hà Nội'),
+							 (2,N'Kim Đồng',N'TP.HCM')
+go
+
+
+Insert Into LoaiSach Values(1,N'Khoa học xã hội'),
+						   (2,N'Toán học'),
+						   (3,N'Tin học')
+go
+
+
+Insert Into Sach Values(1,N'Trí tuệ Do Thái',N'Eran Katz',N'Người Do Thái',2010,1,79000,100,1,1),
+					   (2,N'Giải Toán',N'Katz',N'Toán Cao Cấp',2010,1,60000,100,1,2),
+					   (3,N'Lập Trình Với C',N'Kran Vesson',N'Lập trình',2010,1,80000,100,3,2)
+go
+
+
+-- Liệt kê các cuốn sách có năm xuất bản từ 2008 đến nay
+Select * from Sach
+Where NamXuatBan = 2010
+go
+
+
+-- Liệt kê 10 cuốn sách có giá bán cao nhất
+Select TOP(10) * From Sach
+Where Gia = Max(Gia)
+go
+
+
+-- Tìm những cuốn sách có tiêu đề chứa từ “tin học”
+Select * from Sach
+Where TenSach Like N'%tin học%'
+go
+
+
+-- Liệt kê các cuốn sách có tên bắt đầu với chữ “T” theo thứ tự giá giảm dần
+Select * from Sach 
+Where TenSach Like N'T%'
+Order By Gia DESC
+go
+
+
+-- Liệt kê các cuốn sách của nhà xuất bản Tri thức
+Select TenSach , TenNhaXuatBan
+From Sach INNER JOIN NhaXuatBan
+ON Sach.NhaXuatBanID = NhaXuatBan.NhaXuatBanID
+Where TenNhaXuatBan = N'Tri thức'
+go
+
+
+-- Lấy thông tin chi tiết về nhà xuất bản xuất bản cuốn sách “Trí tuệ Do Thái”
+Select * from NhaXuatBan
+Where NhaXuatBanID = 1
+go
+
+
+-- Hiển thị các thông tin sau về các cuốn sách: Mã sách, Tên sách, Năm xuất bản, Nhà xuất bản, Loại sách
+select * from Sach
+go
+
+
+--  Tìm cuốn sách có giá bán đắt nhất
+Select * from Sach
+Where Gia = Max(Gia)
+go
+
+
+-- Tìm cuốn sách có số lượng lớn nhất trong kho
+Select * from Sach
+Where SoLuong = Max(SoLuong)
+go
+
+
+-- Tìm các cuốn sách của tác giả “Eran Katz”
+Select * from Sach
+Where TacGia = 'Eran Katz'
+go
+
+
+-- Giảm giá bán 10% các cuốn sách xuất bản từ năm 2008 trở về trước
+
+
+-- Thống kê số đầu sách của mỗi loại sách
+Select TenNhaXuatBan, NhaXuatBanID
+From NhaXuatBan
+where NhaXuatBanID IN
+(Select Count(NhaXuatBanID)
+From Sach 
+)
+Group by NhaXuatBanID
